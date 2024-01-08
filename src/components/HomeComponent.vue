b821afda547f8f42f0aa4b3f2ac08d38632544c4
Ekrar Efaz <ekrar@Ekrars-MacBook-Air.local>
2024-01-08 11:33:19 +1100

    Home Page v2

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,15 +1,16 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="6" class="text-md-left text-center">
-        <h1>Hello</h1>
+      <v-col cols="12" md="6" class="text-md-left text-center align-self-center">
+        <!-- Your text content -->
+        <h1>{{ currentGreeting }}</h1>
         <h2>I'm Ekrar Efaz</h2>
         <p>Welcome to my digital domain, where passion for Cyber-Security and mastery in Computer Science converge.</p>
         <v-btn color="grey" dark>Download CV</v-btn>
       </v-col>
-      <v-col cols="12" md="6" class="text-md-right text-center">
-        <!-- Use the imported image here -->
-        <img src="@/assets/profile-pic-nobg.png" class="profile-img"><img>
+      <v-col cols="12" md="6" class="text-md-right text-center align-self-center">
+        <!-- Your image -->
+        <img :src="profilePic" class="profile-img">
       </v-col>
     </v-row>
   </v-container>
@@ -23,7 +24,35 @@ export default {
   data() {
     return {
       profilePic,
+      greetings: [
+        'Hello',    // English
+        'Hola',     // Spanish
+        'Bonjour',  // French
+        'Ciao',     // Italian
+        'Hallo',    // German
+        'Olá',      // Portuguese
+        'Merhaba',  // Turkish
+        'Hej',      // Danish
+        'Hei',      // Norwegian
+        'Sveiki',   // Latvian
+        'Salaam',   // Arabic
+    ],
+      currentGreeting: 'Hello',
+      greetingIndex: 0,
+      interval: null
     }
+  },
+  methods: {
+    changeGreeting() {
+      this.greetingIndex = (this.greetingIndex + 1) % this.greetings.length;
+      this.currentGreeting = this.greetings[this.greetingIndex];
+    }
+  },
+  mounted() {
+    this.interval = setInterval(this.changeGreeting, 4000); // Change every 2000 milliseconds (2 seconds)
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
   },
   name: 'HomePage'
 }
@@ -33,7 +62,6 @@ export default {
 .profile-img {
   max-width: 300px; /* Adjust as needed */
   border-radius: 2%; /* Makes the image round */
-  border: 1px solid #000;
   /* Add object-fit to ensure the image covers the area without distortion */
   object-fit: cover; 
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,3 +15,26 @@ export default {
   }
 }
 </script>
+
+<style>
+  .home{
+  /* Use the full height of the viewport */
+  min-height: 90vh;
+  
+  background-color: antiquewhite;
+
+  /* Set up the background image to cover the entire container */
+  background-size: cover;
+  
+  /* Set the background image to be fixed in the viewport */
+  background-attachment: fixed;
+  
+  /* Position the background image */
+  background-position: center center;
+  
+  /* Ensure the image doesn't repeat */
+  background-repeat: no-repeat;
+
+
+}
+</style>
